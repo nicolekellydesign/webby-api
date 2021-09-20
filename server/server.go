e0985340a4834de100fe24521e11332f50209c8b
Evan Maddock <maddock.evan@vivaldi.net>
2021-09-20 19:13:32 -0400

    Use the RealIP middleware option from go-chi
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- server/server.go
+++ server/server.go
@@ -22,6 +22,7 @@ type Listener struct {
 func New(port int, db *database.DB) *Listener {
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
