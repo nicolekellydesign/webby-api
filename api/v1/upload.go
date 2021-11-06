12b2610544e39e735280cdbf00871b276bb24520
Evan Maddock <maddock.evan@vivaldi.net>
2021-11-06 13:40:41 -0400

    Use Content-Type header to get file path
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

--- api/v1/upload.go
+++ api/v1/upload.go
@@ -5,8 +5,10 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
+// Upload handles requests to upload files to the server.
 func (a API) Upload(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseMultipartForm(8 * 1024 * 1024); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -14,6 +16,7 @@ func (a API) Upload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Get the file from the body
 	file, header, err := r.FormFile("file")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -22,7 +25,15 @@ func (a API) Upload(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
-	outPath := filepath.Join(a.imageDir, header.Filename)
+	// Determine which path the file should be saved to
+	var outPath string
+	if strings.HasPrefix(r.Header.Get("Content-Type"), "image/") {
+		outPath = filepath.Join(a.imageDir, header.Filename)
+	} else {
+		outPath = filepath.Join(a.resourcesDir, header.Filename)
+	}
+
+	// Create our out file
 	out, err := os.Create(outPath)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -31,6 +42,7 @@ func (a API) Upload(w http.ResponseWriter, r *http.Request) {
 	}
 	defer out.Close()
 
+	// Copy the file from the requset to the out file
 	if _, err := io.Copy(out, file); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		a.log.Errorf("error copying to file: %s\n", err.Error())
